8e1338d9153629b554fc19a62ee579d940cf69e6
sai46-ai <23bq1a5446@vvit.net>
2026-06-05 10:58:59 +0530

    main app

--- notification/app.js
+++ notification/app.js
@@ -1,4 +1,19 @@
+const {fetchNotifications}=require("./notificationService");
+const {getTopNotifications}=require("./priorityInbox");
 const logger=require("../logging-middleware/logger");
-logger.info("Application started");
-logger.warn("Testing warning");
-logger.error("Testing error");
+async function run(){
+	try{
+		logger.info("Program started");
+		const data=await fetchNotifications();
+		const topList=getTopNotifications(data,10);
+		console.log("\nTop 10 Unread Notifications\n");
+		topList.forEach((n,i)=>{
+			console.log(`${i+1}. ${n.title} | Priority: ${n.priority} | ${n.timestamp}`);
+		});
+		logger.info("Program finished");
+	}
+	catch(error){
+		logger.error(`Error: ${error.message}`);
+	}
+}
+run();
